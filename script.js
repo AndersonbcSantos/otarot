199d542bf9b402b7af986c185ed24c6afa6c8666
AndersonbcSantos <andersonbritosantos62@gmail.com>
2025-01-24 07:08:37 -0300

    primeira parte site

---
+++ script.js
@@ -0,0 +1,21 @@
+// Função para exibir a transição manual no carrossel de atendentes 
+
+var swiper = new Swiper('.swiper-container', { 
+
+    slidesPerView: 3, 
+
+    spaceBetween: 30, 
+
+    navigation: { 
+
+        nextEl: '.swiper-button-next', 
+
+        prevEl: '.swiper-button-prev', 
+
+    }, 
+
+    loop: true, 
+
+}); 
+
+ 
